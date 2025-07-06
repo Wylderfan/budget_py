1413173408e88c81b0c58263e4854368e6aa1fa1
Wylderfan <pulseconnor@gmail.com>
2025-07-06 09:33:24 -0700

    finished category_db_service.py

--- src/controllers/db/category_db_service.py
+++ src/controllers/db/category_db_service.py
@@ -5,3 +5,91 @@ from database_connector import DatabaseConnector
 class CategoryDBService():
     def __init__(self, db_connector) -> None:
         self.db_connector: DatabaseConnector = db_connector
+
+    def add_category(self, name, category_type):
+        date_created = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        
+        self.db_connector.connect()
+
+        insert_query = """
+        INSERT INTO categories (date_created, name, type)
+        VALUES (%s, %s, %s,)
+        """
+
+        result = self.db_connector.execute_query(
+                insert_query,
+                (date_created, name, category_type)
+                )
+        if result == 1:
+            print("Category has been successfully added")
+        else:
+            print("Error with insert query")
+
+        self.db_connector.close()
+
+    def del_category(self, id):
+        self.db_connector.connect()
+
+        query = """
+        DELETE FROM categories WHERE id = %s
+        """
+
+        result = self.db_connector.execute_query(query, (id,))
+
+        if result == 1:
+            print(f"successfully deleted category id: {id}")
+        else:
+            print(f"Error deleting category")
+
+        self.db_connector.close()
+
+    def search_category(self, id=None, name=None):
+        """Needs either id || name"""
+        self.db_connector.connect()
+
+        if id is not None:
+            query = """
+            SELECT * FROM categories WHERE id = %s
+            """
+        elif name is not None:
+            query = """
+            SELECT * FROM categories WHERE name = %s
+            """
+        else:
+            print("Must use arg id or name")
+            return
+
+        if id is not None:
+            result = self.db_connector.execute_query(query, (id,))
+        else:
+            result = self.db_connector.execute_query(query, (name,))
+
+        self.db_connector.close()
+        
+        return result
+
+    def search_all(self):
+        self.db_connector.connect()
+
+        query = """
+        SELECT * FROM categories
+        """
+
+        result = self.db_connector.execute_query(query)
+
+        self.db_connector.close()
+
+        return result
+
+    def select_name_id_all_categories(self):
+        self.db_connector.connect()
+
+        query = """
+        SELECT name, id FROM categories
+        """
+
+        result = self.db_connector.execute_query(query)
+
+        self.db_connector.close()
+
+        return result
